f77633df869ad24182280feecf0a4058a257b256
Jon Pack <jon.pack@me.com>
2016-08-16 16:42:49 -0400

    Final edits for Choose Your Own Adventure game

--- src/main/java/Validation/ValidationClass.java
+++ src/main/java/Validation/ValidationClass.java
@@ -1,22 +1,44 @@
 package Validation;
 
 public class ValidationClass {
+	int isValid; 
+	
+	
+	
+	public ValidationClass(int isValid) {
+		this.isValid = isValid;
+	}
+	
+	
+	
+	public int getIsValid() {
+		return isValid;
+	}
+
+
+
+	public void setIsValid(int isValid) {
+		this.isValid = isValid;
+	}
+
+
+
 	public static boolean validateNumber3(String userInput) {
+		
+        try {
+            int isValid;
+        	isValid = Integer.parseInt(userInput);
+            if ((isValid < 0) || (isValid > 3)) {
+                System.out.println("You did not enter a number between 1 and 3. Please try again!");
+        	    return true;
+            }
+           
+            return false;
+        }
 
-	       try {
-
-	           int isValid = Integer.parseInt(userInput);
-	           if ((isValid < 0) || (isValid > 3)) {
-	               System.out.println("You did not enter a number between 1 and 3. Please try again!");
-	        	   return true;
-	           }
-	           
-	           return false;
-	       }
-
-	       catch (NumberFormatException e) {
-	    	   return true;
-	       }
+        catch (NumberFormatException e) {
+    	    return true;
+        }
 
 	}
 	
